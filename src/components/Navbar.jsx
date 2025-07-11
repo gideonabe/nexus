ca8cef7c7bfe2038cf2e94b5686b17c9b1742042
Gideon Abe <gideonabe2020@gmail.com>
2025-07-11 19:05:14 +0100

    more commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -72,7 +72,7 @@ const NavBar = () => {
         <nav className="flex size-full items-center justify-between p-4">
           {/* Logo and Product button */}
           <div className="flex items-center gap-7">
-            <img src="/img/logo.png" alt="logo" className="w-10" />
+            <a href="#nexus"><img src="/img/logo.png" alt="logo" className="w-10" /></a>
 
             <Button
               id="product-button"
